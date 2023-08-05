accd40eaf59227f78f5d22d5d90edeba24f0e366
ghos7ie <elyreum7@gmail.com>
2023-08-05 12:01:10 +0800

    Create metronome.java

---
+++ PS0/metronome.java
@@ -0,0 +1,13 @@
+// Problem A
+import java.util.Scanner;
+
+public class metronome {
+
+     public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        
+        double output = (double) input/4;
+        System.out.println(output);
+    }
+}
