687ebec411b07815292272cb2062bad27afd3164
ghos7ie <elyreum7@gmail.com>
2023-08-05 12:39:24 +0800

    Create internationaldates.java

---
+++ PS0/internationaldates.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class internationaldates{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String date = sc.next();
+        // break up the string by /
+        String[] parts = date.split("/");
+        // now loop through each part
+        if (Integer.parseInt(parts[0]) > 12) {
+            System.out.println("EU");
+        }
+        else if (Integer.parseInt(parts[1]) > 12){
+            System.out.println("US");
+        }
+        else{
+            System.out.println("either");
+        }
+        sc.close();
+    }
+}
