7b256560c0cf6bd1b73d5f5293c839a16324530d
ghos7ie <elyreum7@gmail.com>
2023-08-08 15:09:08 +0800

    Create sifferprodukt.java

---
+++ PS0/sifferprodukt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// i have no idea why it is produkt???
+public class sifferprodukt{
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int input = sc.nextInt();
+      System.out.println(produkt(input));
+    }
+    
+    public static int produkt(int input){
+      // stops when the given input is at 10 or less
+      if (input < 10){
+        return input;
+      }
+      int produkt = 1;
+      while (input > 0){
+        // start from last digit since order does not matter
+        // % 10 is used to get last digit
+        produkt *= input % 10 == 0? 1 : input%10;
+        input = input/10;
+      }
+      // will continue to loop with the new number
+      return produkt(produkt);
+    }
+}
