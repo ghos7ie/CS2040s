02dd7ba59bcabcb4b063a662cf7fd61247de6ac7
ghos7ie <elyreum7@gmail.com>
2023-08-08 14:53:02 +0800

    Create undeadoralive.java

---
+++ PS0/undeadoralive.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class undeadoralive {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // I HAD THE RIGHT SOLUTION, BUT I WAS USING SC.NEXT() SO IT ONLY READ UNTIL THE FIRST SPACE
+        String input = sc.nextLine();
+        int smileCount = count(input, ":\\)");
+        int frownCount = count(input, ":\\(");
+        if (smileCount > 0 && frownCount > 0){
+            System.out.println("double agent");
+        }
+        else if (smileCount > 0 && frownCount == 0){
+            System.out.println("alive");
+        }
+        else if (smileCount == 0 && frownCount > 0){
+            System.out.println("undead");
+        }
+        else{
+            System.out.println("machine");
+            
+        }
+    }
+    
+    public static int count(String input, String emoji){
+        return input.split(emoji, -1).length-1;
+    }
+}
