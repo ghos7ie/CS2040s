9d6cfa9e8db5a10ad881846678f3ce048e98f9cc
ghos7ie <elyreum7@gmail.com>
2023-08-09 12:52:36 +0800

    Create babypanda.java

---
+++ PS0/babypanda.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class babypanda {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      // take values in as string and convert as needed later
+      String noOfDays = sc.next();
+      // Slimes present after [noOfDays] night
+      String noOfSlimes = sc.next();
+      // idea: do in binary since if it sneezes, the count will be 0b1
+      // as the slimes continue to split: 0b10 > 0b100 > 0b1000
+      // if the panda sneezes again: 0b101 > 0b1010 > 0b10100
+      // output is number of times panda sneezed
+      
+      // convert to binary
+      BigInteger intNoOfSlimes = new BigInteger(noOfSlimes);
+      String binarySlimes = intNoOfSlimes.toString(2); // Convert to binary
+      int sneezeCount = 0;
+      for(int i = 0, n = binarySlimes.length(); i < n; i++){
+        if (binarySlimes.charAt(i) == '1'){
+          sneezeCount++;
+        }
+      }
+      System.out.println(sneezeCount);
+    }
+}
